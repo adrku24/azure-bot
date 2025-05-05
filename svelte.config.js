881acf2510139939bc6a15b9fbcc24a8dadbd039
Adrian <a@b.c>
2025-05-05 13:35:08 +0200

    Use azure sli for deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,10 @@
-import adapter from '@sveltejs/adapter-node';
+import azure from 'svelte-adapter-azure-swa';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     kit: {
-        adapter: adapter({
-            out: "build"
-        })
+        adapter: azure()
     },
     preprocess: vitePreprocess()
 };
