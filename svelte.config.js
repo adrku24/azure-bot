4cc96df97a82fd8973409a33b6babe2ed560fe6b
Adrian <a@b.c>
2025-05-05 13:22:02 +0200

    Revert to default node adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,14 @@
-// Local: import adapter from '@sveltejs/adapter-node';
-import azure from 'svelte-adapter-azure-swa';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    // Local: adapter: adapter()
-      adapter: azure()
-  },
-  preprocess: vitePreprocess()
+    kit: {
+        adapter: adapter({
+            out: "build"
+        })
+    },
+    preprocess: vitePreprocess()
 };
 
 export default config;
