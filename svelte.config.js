7e4d9490867d6180b02f9e63e542b34d81c72cbc
Adrian <a@b.c>
2025-05-05 12:26:07 +0200

    Azure integration

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,12 @@
-import adapter from '@sveltejs/adapter-node';
+// Local: import adapter from '@sveltejs/adapter-node';
+import azure from 'svelte-adapter-azure-swa';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter()
+    // Local: adapter: adapter()
+      adapter: azure()
   },
   preprocess: vitePreprocess()
 };
