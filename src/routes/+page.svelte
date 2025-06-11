32126709b07edb35d6ed6d9923741d733960803e
Adrian <a.kunze@ifii.eu>
2025-06-11 13:48:29 +0200

    Fixed error in non-loggedin page.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-    let { data, form } = $props();
+    let { form } = $props();
 </script>
 
 <div class="flex h-screen">
@@ -7,7 +7,7 @@
         <p class="block text-gray-700 font-bold text-center text-2xl">Willkommen</p>
         <p class="block text-gray-700 font-bold mb-2 text-center text-sm">Fordere Zugriff auf die Inhalte dieser Seite an.</p>
         <form action="?/signin" method="POST">
-            <div class="flex items-center border-b {data.not_accepted ? 'border-red-500' : 'border-teal-500'} py-2">
+            <div class="flex items-center border-b {form?.not_accepted ? 'border-red-500' : 'border-teal-500'} py-2">
                 <input class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" id="password" type="password" name="password" placeholder="****">
                 <button class="flex-shrink-0 bg-teal-500 hover:bg-teal-800 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="submit">
                     Freischalten
