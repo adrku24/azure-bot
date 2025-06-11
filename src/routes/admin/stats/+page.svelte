399adab7a6ff11457283eda52a6cab2f40077b7b
Adrian <a.kunze@ifii.eu>
2025-06-11 13:38:19 +0200

    Pointy cursor on download statistic hover

--- src/routes/admin/stats/+page.svelte
+++ src/routes/admin/stats/+page.svelte
@@ -37,7 +37,7 @@
 <div class="flex h-screen" id="pdfContent">
     <div class="mt-10 mx-auto mb-auto">
         <div class="flex justify-center mb-15">
-            <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center" onclick={async () => await download()}>
+            <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center hover:cursor-pointer" onclick={async () => await download()}>
                 <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
                 <span>Download</span>
             </button>
