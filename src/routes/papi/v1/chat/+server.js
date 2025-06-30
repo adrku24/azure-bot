f0f31e3876101e08d9042d26db7675365075fa10
adrku24 <a@b.c>
2025-06-30 20:09:53 +0200

    Debug #2

--- src/routes/papi/v1/chat/+server.js
+++ src/routes/papi/v1/chat/+server.js
@@ -13,113 +13,117 @@ import {Unlock} from "$lib/api/unlock/unlock.js";
 const SYSTEM_PROMPT = fs.readFileSync("llm/system/system_prompt.txt", { encoding: 'utf8' });
 
 export async function POST({ request, cookies }) {
-    const accessToken = cookies.get("access_token");
-    if(!accessToken || !(await Unlock.isAccessTokenStillValid(accessToken))) {
-        return text("No Permission.", { status: 403 });
-    }
-
-    const { prompt, conversation } = await request.json();
-
-    if(!prompt) {
-        return json(undefined, { status: 400 });
-    }
-
-    let messages = [];
-    if(conversation !== null && conversation !== undefined && conversation instanceof Array) {
-        for(let i = 0; i < conversation.length; i++) {
-            const role = conversation[i].role;
-            if(role === null || role === undefined) continue;
-
-            const content = conversation[i].content;
-            if(content === null || content === undefined) continue;
-
-            messages.push({
-                "role": role.toUpperCase() === "USER" ? "user" : "assistant",
-                "content": content
-            });
-        }
-    }
-
-    const modifiedSystemPrompt = SYSTEM_PROMPT.replace("{accounts}", JSON.stringify(await UserService.prepareForSystemPrompt()));
-
-    let streamIterator;
     try {
-        streamIterator = await AzureChatGPT.completion(prompt, modifiedSystemPrompt, messages, true);
-    } catch (e) {
-        return text(e.error.message, { status: 400 });
-    }
+        const accessToken = cookies.get("access_token");
+        if (!accessToken || !(await Unlock.isAccessTokenStillValid(accessToken))) {
+            return text("No Permission.", {status: 403});
+        }
 
-    if(streamIterator === null) return json(undefined, { status: 400 });
+        const {prompt, conversation} = await request.json();
 
-    let middleMan = "";
-    const encoder = new TextEncoder();
-    const readableStream = new ReadableStream({
-        async start(controller) {
-            try {
-                let inserted = false;
+        if (!prompt) {
+            return json(undefined, {status: 400});
+        }
 
-                for await (const chunk of streamIterator) {
-                    const content = chunk.choices[0]?.delta?.content;
-                    if (content) {
-                        middleMan = middleMan + content;
-                        controller.enqueue(encoder.encode(content));
-                    } else {
-                        const json = JsonExtractor.extract(middleMan);
-                        for(let i = 0; i < json.length && !inserted; i++) {
-                            const match = json[i];
-                            if(JsonUserMatcher.matches(match)) {
-                                const jsonAddress = match["address"];
+        let messages = [];
+        if (conversation !== null && conversation !== undefined && conversation instanceof Array) {
+            for (let i = 0; i < conversation.length; i++) {
+                const role = conversation[i].role;
+                if (role === null || role === undefined) continue;
 
-                                const address = new UserAddress(
-                                    null,
-                                    jsonAddress["street_address"],
-                                    jsonAddress["city"],
-                                    jsonAddress["state_province"],
-                                    jsonAddress["postal_code"],
-                                    jsonAddress["country"],
-                                    jsonAddress["address_type"]
-                                );
+                const content = conversation[i].content;
+                if (content === null || content === undefined) continue;
 
-                                const jsonPhone = match["phone"];
-                                const phone = new UserPhone(
-                                    null,
-                                    jsonPhone["phone_number"],
-                                    jsonPhone["phone_type"]
-                                );
+                messages.push({
+                    "role": role.toUpperCase() === "USER" ? "user" : "assistant",
+                    "content": content
+                });
+            }
+        }
 
-                                const user = new User(
-                                    null,
-                                    match["first_name"],
-                                    match["last_name"],
-                                    match["birthday"],
-                                    address,
-                                    match["email"],
-                                    phone
-                                );
+        const modifiedSystemPrompt = SYSTEM_PROMPT.replace("{accounts}", JSON.stringify(await UserService.prepareForSystemPrompt()));
 
-                                await UserService.insertUser(user);
-                                controller.enqueue(encoder.encode("!>0<!" + match.confirmation_message));
+        let streamIterator;
+        try {
+            streamIterator = await AzureChatGPT.completion(prompt, modifiedSystemPrompt, messages, true);
+        } catch (e) {
+            return text(e.error.message, {status: 400});
+        }
+
+        if (streamIterator === null) return json(undefined, {status: 400});
+
+        let middleMan = "";
+        const encoder = new TextEncoder();
+        const readableStream = new ReadableStream({
+            async start(controller) {
+                try {
+                    let inserted = false;
+
+                    for await (const chunk of streamIterator) {
+                        const content = chunk.choices[0]?.delta?.content;
+                        if (content) {
+                            middleMan = middleMan + content;
+                            controller.enqueue(encoder.encode(content));
+                        } else {
+                            const json = JsonExtractor.extract(middleMan);
+                            for (let i = 0; i < json.length && !inserted; i++) {
+                                const match = json[i];
+                                if (JsonUserMatcher.matches(match)) {
+                                    const jsonAddress = match["address"];
+
+                                    const address = new UserAddress(
+                                        null,
+                                        jsonAddress["street_address"],
+                                        jsonAddress["city"],
+                                        jsonAddress["state_province"],
+                                        jsonAddress["postal_code"],
+                                        jsonAddress["country"],
+                                        jsonAddress["address_type"]
+                                    );
+
+                                    const jsonPhone = match["phone"];
+                                    const phone = new UserPhone(
+                                        null,
+                                        jsonPhone["phone_number"],
+                                        jsonPhone["phone_type"]
+                                    );
+
+                                    const user = new User(
+                                        null,
+                                        match["first_name"],
+                                        match["last_name"],
+                                        match["birthday"],
+                                        address,
+                                        match["email"],
+                                        phone
+                                    );
+
+                                    await UserService.insertUser(user);
+                                    controller.enqueue(encoder.encode("!>0<!" + match.confirmation_message));
+                                }
                             }
                         }
                     }
+                } catch (error) {
+                    controller.error(error);
+                } finally {
+                    controller.close();
                 }
-            } catch (error) {
-                controller.error(error);
-            } finally {
-                controller.close();
+            },
+
+            cancel() {
+                console.warn("Stream cancelled by client.");
             }
-        },
+        });
 
-        cancel() {
-            console.warn("Stream cancelled by client.");
-        }
-    });
-
-    return new Response(readableStream, {
-        headers: {
-            'Content-Type': 'text/event-stream; charset=utf-8',
-            'Cache-Control': 'no-cache',
-            'X-Content-Type-Options': 'nosniff',
-        }
-    });
+        return new Response(readableStream, {
+            headers: {
+                'Content-Type': 'text/event-stream; charset=utf-8',
+                'Cache-Control': 'no-cache',
+                'X-Content-Type-Options': 'nosniff',
+            }
+        });
+    } catch (e) {
+        return text(e.error.message, {status: 400});
+    }
 }
